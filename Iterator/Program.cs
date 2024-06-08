1c6a74acd78b93e85aa9054424c9b26c87e01c5f
Harunoshin Fujita <fujita.harunoshin@gmail.com>
2024-06-08 15:58:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Iterator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Iterator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var collection = new WordsCollection();
+        collection.AddItem("First");
+        collection.AddItem("Second");
+        collection.AddItem("Third");
+
+        Console.WriteLine("Straight traversal:");
+
+        foreach (var element in collection)
+        {
+            Console.WriteLine(element);
+        }
+
+        Console.WriteLine("\nReverse traversal:");
+
+        collection.ReverseDirection();
+
+        foreach (var element in collection)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
---
+++ Iterator/WordsCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Iterator;
+
+internal class WordsCollection : IteratorAggregate
+{
+    List<string> _collection = [];
+
+    bool _direction = false;
+
+    public void ReverseDirection()
+    {
+        _direction = !_direction;
+    }
+
+    public List<string> getItems()
+    {
+        return _collection;
+    }
+
+    public void AddItem(string item)
+    {
+        _collection.Add(item);
+    }
+
+    /// <summary>
+    /// クライアント側でforeachを使用したときに参照される
+    /// </summary>
+    public override IEnumerator GetEnumerator()
+    {
+        return new AlphabeticalOrderIterator(this, _direction);
+    }
+}
